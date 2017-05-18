80ef929cb216f2cb3e679a8583abc59f600b2b22
Song Hyeon Sik <blogcin@naver.com>
2017-05-18 17:26:27 +0900

    장고 버전 설정 및 그래프 조절
    Signed-off-by: Song Hyeon Sik <blogcin@naver.com>

--- wargame/urls.py
+++ wargame/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 	url(r'^user/', include('userAccount.urls', namespace='user')),
-	url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+	url(r'^media/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT}),
 ]
